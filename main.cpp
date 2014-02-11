495214ed22fcbab627513dcde6149c3edf93982e
Alphawolfdeveloper <rbwolfboy@gmail.com>
2014-02-11 17:25:27 -0400

    testing
    C++

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rafaelbeauchamp
+ *
+ * Created on February 11, 2014, 5:18 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+    cout << "Hello" << endl;
+
+    return 0;
+}
+
